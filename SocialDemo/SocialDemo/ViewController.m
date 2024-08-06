3c3d2f1b3213fb76bb8690a11a5e0338c22504e0
wongbingg <wb277@naver.com>
2024-08-06 13:18:43 +0900

    refactor: ViewController 코드정리 및 오토레이아웃 설정

--- SocialDemo/SocialDemo/ViewController.m
+++ SocialDemo/SocialDemo/ViewController.m
@@ -17,51 +17,88 @@
 
 @implementation ViewController
 
+// MARK: - View LifeCycle
 - (void)viewDidLoad {
   [super viewDidLoad];
   
   self.view.backgroundColor = [UIColor whiteColor];
   
+  [self setupLabel];
+  [self setupButton];
+  [self setupViewHierarchy];
+  [self setupViewConstraints];
+}
+
+// MARK: - Methods
+- (void)loginWithKakaoTalkApp {
+  [[KakaoSDKUserWrapper shared]
+   loginWithKakaoTalkWithChannelPublicIds:nil
+   serviceTerms:nil
+   nonce:nil
+   completion:^(NSDictionary * _Nullable tokenInfo, NSError * _Nullable error) {
+    if (error) {
+      NSLog(@"Error: %@", error.localizedDescription);
+    } else {
+      NSLog(@"loginWithKakaoTalk() success.");
+      NSLog(@"Token Info: %@", tokenInfo);
+    }
+  }];
+}
+
+- (void)loginWithKakaoAccount {
+  [[KakaoSDKUserWrapper shared]
+   loginWithKakaoAccountWithPrompts:nil
+   loginHint:nil
+   completion:^(NSDictionary<NSString *,id> * _Nullable oauthToken, NSError * _Nullable error) {
+    if (error) {
+      NSLog(@"Error: %@", error.localizedDescription);
+    } else {
+      NSLog(@"loginWithKakaoAccount() success.");
+      NSLog(@"oauthToken Info: %@", oauthToken);
+    }
+  }];
+}
+
+// MARK: - UI Setup
+- (void)setupLabel {
   self.label = [[UILabel alloc] init];
   self.label.text = @"Hello, World!";
   self.label.textAlignment = NSTextAlignmentCenter;
   self.label.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.view addSubview:self.label];
-  
-  [NSLayoutConstraint activateConstraints:@[
-    [self.label.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
-    [self.label.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor]
-  ]];
-  
+}
+
+- (void)setupButton {
   self.loginButton = [UIButton buttonWithType:UIButtonTypeSystem];
-  self.loginButton.frame = CGRectMake(100, 100, 200, 50);
+  self.loginButton.translatesAutoresizingMaskIntoConstraints = NO;
   [self.loginButton setTitle:@"카카오로그인" forState:UIControlStateNormal];
   [self.loginButton addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
-  [self.view addSubview:self.loginButton];
 }
 
 - (void)buttonTapped:(UIButton *)sender {
   NSLog(@"버튼이 클릭되었습니다!");
   if ([KakaoSDKUserWrapper isKakaoTalkLoginAvailable]) {
-    [[KakaoSDKUserWrapper shared] loginWithKakaoTalkWithChannelPublicIds:nil serviceTerms:nil nonce:nil completion:^(NSDictionary * _Nullable tokenInfo, NSError * _Nullable error) {
-      if (error) {
-        NSLog(@"Error: %@", error.localizedDescription);
-      } else {
-        NSLog(@"loginWithKakaoTalk() success.");
-        NSLog(@"Token Info: %@", tokenInfo);
-      }
-    }];
+    [self loginWithKakaoTalkApp];
   } else {
     // 카카오톡이 설치되어있지 않은 경우
-    [[KakaoSDKUserWrapper shared] loginWithKakaoAccountWithPrompts:nil loginHint:nil completion:^(NSDictionary<NSString *,id> * _Nullable oauthToken, NSError * _Nullable error) {
-      if (error) {
-        NSLog(@"Error: %@", error.localizedDescription);
-      } else {
-        NSLog(@"loginWithKakaoAccount() success.");
-        NSLog(@"oauthToken Info: %@", oauthToken);
-      }
-    }];
+    [self loginWithKakaoAccount];
   }
+}
+
+- (void)setupViewHierarchy {
+  [self.view addSubview:self.label];
+  [self.view addSubview:self.loginButton];
+}
+
+- (void)setupViewConstraints {
+  [NSLayoutConstraint activateConstraints:@[
+    [self.label.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+    [self.label.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor]
+  ]];
+  
+  [NSLayoutConstraint activateConstraints:@[
+    [self.loginButton.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor],
+    [self.loginButton.topAnchor constraintEqualToAnchor:self.label.bottomAnchor constant:20],
+  ]];
 }
 
 @end
